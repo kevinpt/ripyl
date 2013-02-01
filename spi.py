6fe1efbef3af0c76eea23b22d9bec1b5348b8e4f
kevinpt <kevinpt@Vulcan>
2013-02-01 01:36:43 -0500

    Removed old decoder

--- spi.py
+++ spi.py
@@ -38,12 +38,13 @@ class SpiFrame(StreamSegment):
     def __str__(self):
         return str(self.data)
 
-def spi_decode_2(clk, mosi, cs=None, cpol=0, cpha=0, lsb_first=True, stream_type=StreamType.Samples):
+def spi_decode(clk, mosi, cs=None, cpol=0, cpha=0, lsb_first=True, stream_type=StreamType.Samples):
     if stream_type == StreamType.Samples:
         # tee off an iterator to determine logic thresholds
         s_clk_it, thresh_it = itertools.tee(clk)
         
         logic = find_logic_levels(thresh_it, max_samples=5000)
+        #print('     LOGIC>>>> ', logic)
         if logic is None:
             raise StreamError('Unable to find avg. logic levels of waveform')
         del thresh_it
@@ -87,28 +88,17 @@ def spi_decode_2(clk, mosi, cs=None, cpol=0, cpha=0, lsb_first=True, stream_type
     # begin to decode
     
     while not es.at_end():
-        #start_time = es.cur_time()
-        
-        ctb = es.cur_time()
-        
         ts, cname = es.advance_to_edge()
-        
-        # if ts == 0.0:
-            # print('>>>', ts, cname)
-            # break
-            
-        print(' ## ts={0}, {1}, ctb={2}, cts={3}'.format(ts, cname, ctb, es.cur_time()))
+
         
         if cname == 'cs' and not es.at_end('cs'):
-            #print('CS event')
-            print('    CS **', es.cur_time())
-            pass # FIX: generate CS event
-            
+            se = StreamEvent(es.cur_time(), data=es.cur_state('cs'), kind='SPI CS')
+            yield se
+
         elif cname == 'clk' and not es.at_end('clk'):
             clk_val = es.cur_state('clk')
             
             if clk_val == active_edge: # capture data bit
-                print('    **', es.cur_time())
                 # Check if the elapsed time is more than any previous cycle
                 # This indicates that a previous word was complete
                 if not prev_cycle is None and es.cur_time() - prev_edge > 1.05 * prev_cycle:
@@ -130,7 +120,6 @@ def spi_decode_2(clk, mosi, cs=None, cpol=0, cpha=0, lsb_first=True, stream_type
                     
                 bits.append(es.cur_state('mosi'))
                 end_time = es.cur_time()
-                #print('>> et', end_time, bits)
                 
                 if not prev_edge is None:
                     prev_cycle = es.cur_time() - prev_edge
@@ -139,6 +128,7 @@ def spi_decode_2(clk, mosi, cs=None, cpol=0, cpha=0, lsb_first=True, stream_type
                 
         
     if len(bits) > 0:
+        #print('  >>> bits:', len(bits))
         word = 0
         for b in bits[::step]:
             word = word << 1 | b
@@ -151,106 +141,6 @@ def spi_decode_2(clk, mosi, cs=None, cpol=0, cpha=0, lsb_first=True, stream_type
 
     
         
-def spi_decode(clk, mosi, cs, cpol=0, cpha=0):
-    edge_sets = {
-        'clk': clk,
-        'mosi': mosi,
-        'cs': cs
-    }
-    
-    es = multi_edge_sequence(edge_sets, 0.0)
-    
-    prev_ts, _ = es.advance_to_edge('clk') # move to first edge of clock
-    
-    if cpha == 1:
-        es.advance_to_edge('clk')
-    
-    end_decode = False
-    #w_num = 0
-    while not es.at_end():
-        #if cpha == 1: # skip first edge of clock
-        #    ts = es.advance_to_edge('clk')
-        #    if ts == 0.0:
-        #        break
-        
-        start_time = es.cur_time()
-        start_cs = es.cur_state('cs')
-        
-        #b_num = 0
-        bits = []
-        while True:
-            bits.append(es.cur_state('mosi'))
-            #print("# w{0} b{1}: '{2}',  t: {3}".format(w_num, b_num, bits[-1], es.cur_time()))
-            #b_num += 1
-            
-            end_time = es.cur_time()
-        
-            ts, _ = es.advance_to_edge('clk')
-            if ts == 0.0: # no more clock edges
-                end_decode = True
-                break
-            
-            ts2, _ = es.advance_to_edge('clk')
-            if ts2 == 0.0: # no more clock edges
-                end_decode = True
-                break
-            ts += ts2
-
-                
-            if ts > prev_ts * 1.05:
-                # the word ended
-                break
-                
-            prev_ts = ts
-            
-        prev_ts = ts
-        #w_num += 1
-        
-        word = 0
-        for b in bits[::-1]:
-            word = word << 1 | b
-        
-        nf = SpiFrame((start_time, end_time), word)
-        nf.word_size = len(bits)
-        nf.cs = start_cs
-        
-        yield nf
-        
-        if end_decode:
-            break
-            
-        
-            
-        # if ts > prev_ts * 1.05:
-            # # the word has ended
-            # word = 0
-            # for b in bits[::-1]:
-                # word = word << 1 | b
-            
-            # start_time = 0
-            # end_time = 0
-            # nf = spi_frame((start_time, end_time), word)
-            
-            # yield nf
-            # bits = []
-
-        # prev_ts = ts
-            
-        # print(es.cur_state(), ts, es.sequences[0].cur_time)
-    
-    # # last word
-    # if len(bits) > 0:
-        # word = 0
-        # for b in bits[::-1]:
-            # word = word << 1 | b
-        # nf = spi_frame((start_time, end_time), word)            
-        # yield (nf, bits)
-        
-    #ae = [s.at_end() for s in es.sequences]
-    #print(ae)
-    #print(len(bits))
-
-
 def spi_synth(data, word_size, clock_freq, cpol=0, cpha=0, lsb_first=True, idle_start=0.0, word_interval=0.0):
         t = 0.0
         clk = cpol
@@ -266,20 +156,18 @@ def spi_synth(data, word_size, clock_freq, cpol=0, cpha=0, lsb_first=True, idle_
         for i, d in enumerate(data):
             bits_remaining = word_size
             bits = [0] * word_size
-            for i in xrange(word_size):
+            for j in xrange(word_size):
                 b = d & 0x01
                 d = d >> 1
                 
                 # first bit transmitted will be at the end of the list
                 if lsb_first:
-                    bits[word_size - 1 - i] = b
+                    bits[word_size - 1 - j] = b
                 else:
-                    bits[i] = b
+                    bits[j] = b
             
             if cpha == 0: # data goes valid a half cycle before the first clock edge
                 t += half_bit_period
-                #mosi = d & 0x01
-                #d = d >> 1
                 mosi = bits[bits_remaining-1]
                 bits_remaining -= 1
                 cs = 0
@@ -295,8 +183,6 @@ def spi_synth(data, word_size, clock_freq, cpol=0, cpha=0, lsb_first=True, idle_
             while bits_remaining:
                 t += half_bit_period
                 clk = 1 - clk
-                #mosi = d & 0x01
-                #d = d >> 1
                 mosi = bits[bits_remaining-1]
                 bits_remaining -= 1
                 yield ((t, clk),(t, mosi),(t, cs))
