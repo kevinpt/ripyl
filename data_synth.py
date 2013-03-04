90085960454a0768273f42f7192b9753a777470c
kevinpt <kevinpt@Vulcan>
2013-03-03 19:20:50 -0500

    Add USB support

--- data_synth.py
+++ data_synth.py
@@ -12,6 +12,8 @@ import matplotlib.patches as pts
 
 import numpy as np
 
+from collections import deque
+
 from decode import *
 from streaming import *
 from sigproc import *
@@ -22,25 +24,220 @@ from protocol.usb import *
 from stats import OnlineStats
 
 
+def read_bin_file(fname):
+    import struct
+    with open(fname, 'rb') as fo:
+        sample_period = struct.unpack('<d', fo.read(8))[0]
+        samples = []
+        while True:
+            s = fo.read(8)
+            if len(s) > 0:
+                samples.append(struct.unpack('<d', s)[0])
+            else:
+                break
+    
+        return (samples, sample_period)
+
+        
+def remove_short_se0s(diff_edges, min_se0_time):
+    se0_start = None
+    
+    # Don't filter the first edge
+    try:
+        edge = next(diff_edges)
+    except StopIteration:
+        raise StopIteration #FIX
+        
+    #yield edge
+    
+    merged_edge = False
+    while True:
+        prev_edge = edge
+        try:
+            edge = next(diff_edges)
+        except StopIteration:
+            break
+            
+        if edge[1] != 0:
+            merged_edge = False
+
+        if se0_start is not None: # prev edge started SE0
+            se0_end = edge[0]
+            se0_len = se0_end - se0_start
+            
+            if se0_len < min_se0_time:
+                # merge edges and remove SE0
+                prev_edge = ((se0_start + se0_end) / 2.0, edge[1])
+                merged_edge = True
+                yield prev_edge
+            else:
+                merged_edge = False
+
+            se0_start = None
+
+        if edge[1] == 0: #SE0
+            se0_start = edge[0]
+
+        if not merged_edge:
+            yield prev_edge
+
+    yield prev_edge # last edge
+            
+        
+def test_diff_usb_file():
+    dp_samples, sample_period = read_bin_file('test/data/usb_dp.bin')
+    dm_samples, sample_period = read_bin_file('test/data/usb_dm.bin')
+    time_samples = [i*sample_period for i in xrange(len(dp_samples))]
+    
+    # generate differential waveform
+    d_diff_samples = [s[0] - s[1] for s in zip(dp_samples, dm_samples)]
+    
+    dp_s = zip(time_samples, dp_samples)
+    dm_s = zip(time_samples, dm_samples)
+    
+    d_diff_s = zip(time_samples, d_diff_samples)
+    
+    
+    plt.clf()
+    dp_s2 = [s + 4.0 for s in dp_samples]
+    plt.plot(time_samples, dp_s2)
+    plt.plot(time_samples, dm_samples)
+    
+    plt.plot(time_samples, d_diff_samples)
+
+
+    # generate single-ended edge waveforms
+    logic = find_logic_levels(iter(dp_s), max_samples=5000, buf_size=2000)
+    hyst = 0.4
+    dp_e = list(find_edges(iter(dp_s), logic, hysteresis=hyst))
+    dm_e = list(find_edges(iter(dm_s), logic, hysteresis=hyst))
+    
+    dp_e_t, dp_e_wf = zip(*list(sample_edge_list(iter(dp_e), sample_period, 1.0e-6)))
+    dm_e_t, dm_e_wf = zip(*list(sample_edge_list(iter(dm_e), sample_period, 1.0e-6)))
+    
+    plt.plot(dp_e_t, dp_e_wf)
+    plt.plot(dm_e_t, dm_e_wf)
+    
+    # generate differential edge waveform
+    d_logic = find_logic_levels(iter(d_diff_s), max_samples=5000, buf_size=2000)
+    hyst = 0.1
+    d_diff_e = list(find_differential_edges(iter(d_diff_s), d_logic, hysteresis=hyst))
+    print('DIFF edges', d_diff_e, '\n')
+    
+    min_se0 = 60.0e-9
+    filt1 = list(remove_short_se0s(iter(d_diff_e), min_se0))
+    print('FILTER:', list(iter(filt1)), '\n')
+    print('FILTER 2:', list(remove_short_se0s(iter(filt1), min_se0)), '\n')
+    
+    d_diff_e = list(remove_short_se0s(remove_short_se0s(iter(d_diff_e), min_se0), min_se0))
+    #print('FILTERED edges', d_diff_e)
+    
+    dd_e_t, dd_e_wf = zip(*list(sample_edge_list(iter(d_diff_e), sample_period, 1.0e-6)))
+    
+    plt.plot(dd_e_t, dd_e_wf)
+    
+    #print('logic', logic)
+    #print('dp_e', dp_e)
+    #print('dm_e', dm_e)
+    
+    plt.show()
+    
+    records_it = usb_diff_decode(iter(d_diff_e), stream_type=StreamType.Edges)
+    records = list(records_it)
+    
+    print('*************** decoded records', len(records))
+    for r in records:
+        if r.kind == 'USB packet':
+            print('{}, {}'.format((r.start_time, r.end_time), r.packet))
+        elif r.kind == 'USB error':
+            print('{}, {}'.format((r.start_time, r.end_time), r))
+    
+def test_usb_file():
+    dp_samples, sample_period = read_bin_file('test/data/usb_dp.bin')
+    dm_samples, sample_period = read_bin_file('test/data/usb_dm.bin')
+    time_samples = [i*sample_period for i in xrange(len(dp_samples))]
+    
+    dp_s = zip(time_samples, dp_samples)
+    dm_s = zip(time_samples, dm_samples)
+    
+    
+    plt.clf()
+    dp_s2 = [s + 4.0 for s in dp_samples]
+    plt.plot(time_samples, dp_s2)
+    plt.plot(time_samples, dm_samples)
+
+    
+    logic = find_logic_levels(iter(dp_s), max_samples=5000, buf_size=2000)
+    hyst = 0.4
+    dp_e = list(find_edges(iter(dp_s), logic, hysteresis=hyst))
+    dm_e = list(find_edges(iter(dm_s), logic, hysteresis=hyst))
+    
+    dp_e_t, dp_e_wf = zip(*list(sample_edge_list(iter(dp_e), sample_period, 1.0e-6)))
+    dm_e_t, dm_e_wf = zip(*list(sample_edge_list(iter(dm_e), sample_period, 1.0e-6)))
+    
+    plt.plot(dp_e_t, dp_e_wf)
+    plt.plot(dm_e_t, dm_e_wf)
+    
+    #print('logic', logic)
+    #print('dp_e', dp_e)
+    #print('dm_e', dm_e)
+    
+    plt.show()
+    
+    
+    records_it = usb_decode(iter(dp_s), iter(dm_s), stream_type=StreamType.Samples)
+    records = list(records_it)
+    
+    print('*************** decoded records', len(records))
+    for r in records:
+        if r.kind == 'USB packet':
+            print('{}, {}'.format((r.start_time, r.end_time), r.packet))
+        elif r.kind == 'USB error':
+            print('{}, {}'.format((r.start_time, r.end_time), r))
+
+
 def test_usb():
-    p = USBTokenPacket(USBPID.TokenIn, 123, 1)
-    p2 = USBTokenPacket(USBPID.TokenIn, 124, 1, delay=2.0e-7)
+    speed = USBSpeed.FullSpeed
+
+    p = USBTokenPacket(USBPID.TokenIn, 0x7F, 1, speed=speed)
+    p2 = USBTokenPacket(USBPID.TokenIn, 124, 1, delay=30.0e-7, speed=speed)
+    p3 = USBDataPacket(USBPID.Data0, [1,2,3,4,5,0xff, 0xff, 0xff], speed=speed)
+    p4 = USBHandshakePacket(USBPID.ACK, speed=speed)
+    p5 = USBSOFPacket(USBPID.SOF, 0x123, speed=speed)
+    p6 = USBEXTPacket(USBPID.EXT, 0x22, 0x03, 0xA, 0x1A0, speed=speed)
+    p7 = USBSplitPacket(USBPID.SPLIT, 0x11, 0, 0x10, 1, 0, 0x1, speed=speed)
     
-    #print(p.GetEdges()[1])
+    p4.hs_sync_dropped_bits = 18
     
-    packets = [p, p2]
+    #packets = [p, p2, p3, p4, p5, p6]
+    
+    packets = [p, p2, p4, p5, p3]
+    packets = [p, p2, p6, p5]
+    packets = [p4, p4]
     
     dp, dm = zip(*list(usb_synth(packets, 1.0e-7, 3.0e-7)))
     
     dp = list(remove_excess_edges(iter(dp)))
     dm = list(remove_excess_edges(iter(dm)))
     
-    sr = 1.0 / (12.0e6 * 20.0)
+    sr = USBClockPeriod[speed] / 10.0
     dm_t, dm_wf = zip(*list(sample_edge_list(iter(dm), sr)))
     dp_t, dp_wf = zip(*list(amplify(sample_edge_list(iter(dp), sr), 1.0, 1.1)))
     
+    records_it = usb_decode(iter(dp), iter(dm), stream_type=StreamType.Edges)
+    
+    records = list(records_it)
+    
+    print('*************** decoded records', len(records))
+    for r in records:
+        if r.kind == 'USB packet':
+            print('{}, {}'.format((r.start_time, r.end_time), r.packet))
+        elif r.kind == 'USB error':
+            print('{}, {}'.format((r.start_time, r.end_time), r))
+    
+    
     # create differential signal
-    d_diff = list(sum(iter(zip(dp_t, dp_wf)), invert(iter(zip(dm_t, dm_wf))) ))
+    d_diff = list(sum_streams(iter(zip(dp_t, dp_wf)), invert(iter(zip(dm_t, dm_wf))) ))
     d_diff_t, d_diff_wf = zip(*list(amplify(iter(d_diff), 0.2, 0.5)))
     
     
@@ -83,8 +280,10 @@ def test_i2c():
 
 def test_spi():
     cpol = 0
-    cpha = 0
-    clk, mosi, cs = zip(*list(spi_synth([3, 4, 5, 240], 8, 1.0, cpol, cpha, True, 2.0, 0.0)))
+    cpha = 1
+    clock_freq = 30.0
+    
+    clk, mosi, cs = zip(*list(spi_synth([3, 4, 5, 240], 8, clock_freq, cpol, cpha, True, 4.0 / clock_freq, 0.0)))
     
     #print('CLK:', clk)
     #print( 'MOSI:', mosi)
@@ -93,7 +292,7 @@ def test_spi():
     mosi = list(remove_excess_edges(iter(mosi)))
     cs = list(remove_excess_edges(iter(cs)))
     
-    use_edges=True
+    use_edges=False
     
 
     if use_edges:
@@ -117,7 +316,8 @@ def test_spi():
     else:
     
         #frames = spi_decode_2(iter(clk), iter(mosi), iter(cs), cpol=cpol, cpha=cpha, lsb_first=True)
-        sr = 1.0e-2
+        sr = 10.0e-2
+        sr = 1.0 / (20.0 * clock_freq)
         # #print('>>>>>', len(list(sample_edge_list(iter(clk), sr))))
         # ct, clk_wf = zip(*list(sample_edge_list(iter(clk), sr)))
         # print('logic>', find_logic_levels(noisify(sample_edge_list(iter(clk), sr)), max_samples=10000))
@@ -125,13 +325,13 @@ def test_spi():
         # plt.show()
 
         clk_s = list(sample_edge_list(iter(clk), sr))
-        #clk_s = list(noisify(sample_edge_list(iter(clk), sr), 15))
+        #clk_s = list(noisify(sample_edge_list(iter(clk), sr), 30.0))
         mosi_s = list(noisify(sample_edge_list(iter(mosi), sr)))
         cs_s = list(noisify(sample_edge_list(iter(cs), sr)))
         
         #print('                  SEL >>> ', cs_s[-1])
         
-        records = spi_decode_2(iter(clk_s), iter(mosi_s), iter(cs_s), cpol=cpol, cpha=cpha, lsb_first=True)
+        records = spi_decode(iter(clk_s), iter(mosi_s), iter(cs_s), cpol=cpol, cpha=cpha, lsb_first=True)
         
         records = list(records)
         
@@ -139,7 +339,7 @@ def test_spi():
         mt, mosi_wf = zip(*mosi_s)
         st, cs_wf = zip (*cs_s)
         
-        logic = find_logic_levels(iter(clk_s), max_samples=5000)
+        logic = find_logic_levels(iter(clk_s), max_samples=5000, buf_size=2000)
         clk_e = list(find_edges(iter(clk_s), logic, hysteresis=0.4))
         mosi_e = list(find_edges(iter(mosi_s), logic, hysteresis=0.4))
         cs_e =   list(find_edges(iter(cs_s), logic, hysteresis=0.4))
@@ -175,22 +375,47 @@ def test_spi():
 
 def test_uart():
 
-    rise_time = 1.0e-6
-    sample_rate = 10.0e6
+    #rise_time = 1.0e-6
+    #sample_rate = 10.0e6
+    
     #sample_rate = 4.0 / rise_time
-    print('sample rate', sample_rate)
+    
+    
     # edge_bw = 0.35 / rise_time
     
     # print('edge bandwidth: {0:,}'.format(edge_bw))
     
-    #edges = serial_synth('Hello world!', 115200.0)
-    #edges = uart_synth('He', 57600.0)
-    edges = uart_synth(bytearray('Hello world???'), 8, 115200.0, parity='even', idle_start=100.0e-7)
+    msg = '9rYQ\_R;N\J\0I`v8\cR'
+    baud = 128000
+    parity=None
+    bits=8
+    
+    msg = 'iJ[hOH5H<4Cl@q_wW?ss'
+    baud = 460800
+    parity = 'odd'
+    bits = 7
+    
+    msg = 'L`kVRZn1M60YLO@c?4un'
+    #msg = 'L`kVRZ'
+    baud = 110
+    parity = None
+    bits = 8
+    
+    sample_rate = baud * 100.0
+    rise_time = 0.35 * 2.0 / sample_rate * 10.0 # 10x min rise time
+    
+    print('sample rate', sample_rate)
+    
+    edges = uart_synth(bytearray(msg), bits, baud, parity=parity, idle_start=100.0 / sample_rate)
+    elist = list(edges)
+    print('elist len', len(elist))
+    print('elist:', elist)
+    edges = iter(elist)
     
     
     samples = synth_wave(edges, sample_rate, rise_time, ripple_db=60)
     
-    noisy = amplify(noisify(samples, snr_db=20.0), gain=-15.0)
+    noisy = amplify(noisify(samples, snr_db=30.0), gain=-15.0)
     
     #t = np.arange(len(noisy)) / sample_rate
     
@@ -226,14 +451,23 @@ def test_uart():
             # os.reset()
 
     
-    frames = uart_decode(iter(waveform), inverted=False, parity='even', baud_rate=None)
+    bd = deque()
+    frames = uart_decode(iter(waveform), bits=bits, inverted=False, parity=parity, baud_rate=None, baud_deque=bd)
+    #frames = uart_decode(iter(waveform), bits=bits, inverted=False, parity=parity, baud_rate=baud, baud_deque=bd)
+    #frames = uart_decode(iter(elist), bits=bits, inverted=True, parity=parity, baud_rate=baud, baud_deque=bd, stream_type=StreamType.Edges)
     frames = list(frames)
     print(''.join(str(d) for d in frames))
+    bd = bd.pop()
+    print(bd)
+    print('edges len', len(bd['edges']))
 
     plt.plot(t, wf)
     # plt.plot(t, sd_wf)
     # plt.plot(t, m_wf)
     ax = plt.axes()
+    
+    elt, elwf = zip(*elist)
+    plt.plot(elt, elwf)
     
     
     logic = find_logic_levels(iter(waveform), 5000, 2000)
@@ -246,6 +480,8 @@ def test_uart():
     #plt.ylim(rect_bot - span * 0.1, rect_top + span * 0.1)
     
     print('Logic levels:', logic)
+    
+    print('frames:', frames)
     
     for f in frames:
         plt.text((f.start_time + f.end_time) / 2.0, text_height, str(f))
@@ -286,192 +522,6 @@ def test_uart():
     #plt.show()
     
         
-    
-
-# def sample_edge_list(edges, sample_period):
-    # t = 0.0
-    
-    # try:
-        # cur_states = next(edges)
-        # next_states = next(edges)
-    # except StopIteration:
-        # raise StreamError('Not enough edges to generate samples')
-    
-    # t = cur_states[0]
-
-    # while True:
-        # while t < next_states[0]:
-            # yield (t, cur_states[1])
-            # t += sample_period
-        
-        # cur_states = next_states
-        # try:
-            # next_states = next(edges)
-        # except StopIteration:
-            # break
-
-    
-    
-# def filter_edges(samples, sample_rate, rise_time, ripple_db=60.0, pool_size=1000):
-    # nyquist = sample_rate / 2.0
-    # edge_bw = 0.35 / rise_time
-    # transition_bw = edge_bw * 4.0 # this gives a nice smooth transition
-    # cutoff_hz = edge_bw
-    
-    # if cutoff_hz > nyquist:
-        # min_rise = 0.35 / nyquist
-        # raise ValueError('Rise time is too fast for current sample rate (min: {0})'.format(min_rise))
-    
-    # #print('nyquist', nyquist, 'transition_bw', transition_bw, 'cutoff_hz', cutoff_hz)
-    
-    # N, beta = signal.kaiserord(ripple_db, transition_bw / nyquist)
-    
-    # #print('cutoff', cutoff_hz, 'nyq', nyquist, cutoff_hz / nyquist)
-    
-    # taps = signal.firwin(N, cutoff_hz / nyquist, window=('kaiser', beta))
-    # print(len(taps), N)
-    
-    # # filter delay
-    # delay = 0.5 * (N-1) / sample_rate
-    # print('DE', delay)
-    
-    # if pool_size < 2*N:
-        # pool_size = 2*N
-
-    # stream_ended = False
-
-    # # get a pool of samples
-    # spool = np.zeros((pool_size + N-1,), dtype = np.float)
-    # samp_it, init_it = itertools.tee(samples)
-    # spool[0:N//2-1] += next(init_it)[1] # pad the first part of the pool with a copy of the first sample
-    # del init_it
-    
-    # tpool = np.zeros((pool_size + N-1,), dtype = np.float)
-    
-    # # prime the initial portion of the pool with data that will be filtered out
-    # for i in xrange(N//2 - 1, N-1):
-        # try:
-            # tpool[i], spool[i] = next(samp_it)
-        # except StopIteration:
-            # stream_ended = True
-            # break
-    
-    # valid_samples = 0
-    
-    # while not stream_ended:
-        # for i in xrange(N-1, pool_size + N-1):
-            # try:
-                # tpool[i], spool[i] = next(samp_it)
-                # valid_samples = i
-            # except StopIteration:
-                # stream_ended = True
-                # break
-                
-        # #print('VS', valid_samples, len(tpool))
-        
-        # filt = signal.lfilter(taps, 1.0, spool[:valid_samples+1])
-        # #print(len(filt), pool_size +N-1)
-        # ##tpool -= delay
-        
-        # # copy end samples to start of pool
-        # if not stream_ended:
-            # spool[0:N-1] = spool[pool_size:pool_size + N-1] #filt[pool_size:pool_size + N-1]
-        # #tpool[0:N] = tpool[pool_size - 1:pool_size + N-1]
-        
-        # tpool[N-1:] -= delay
-        # #print('TP', tpool[N-1])
-        
-        # for i in xrange(N-1, valid_samples+1):
-            # yield (tpool[i], filt[i])
-        
-
-
-# def add_noise(samples, snr_db=30.0):
-    # if snr_db > 80.0:
-        # return samples
-    # else:
-        # noise_sd = 0.5 / (10.0 ** (snr_db / 20.0))
-        # noise = np.random.normal(0.0, noise_sd, len(samples))
-        # return samples + noise
-
-        
-
-# def synth_wave(edges, sample_rate, rise_time, ripple_db=60.0):
-    # sample_period = 1.0 / sample_rate
-    
-    # samples = sample_edge_list(edges, sample_period)
-    
-    # # ot, osamples = zip(*samples)
-    # # samples = iter(zip(ot, osamples))
-    # # ofiltered, ot = filter_edges(osamples, sample_rate, rise_time, ripple_db)
-
-    # # plt.plot(ot, ofiltered + 0.005)
-    
-    # # t, filtered = zip(*filter_edges_2(samples, sample_rate, rise_time, ripple_db))
-    
-    # # print('##', len(ofiltered), len(filtered))
-    # # pj = 0.0
-    # # for i, j in enumerate(t):
-        # # if j - pj > (1.1  / sample_rate):
-            # # print('#### missing sample:', i, j)
-        # # pj = j
-    
-    # # plt.plot(t, filtered)
-    # # plt.ylim(-0.05, 1.05)
-    # # plt.show()
-    
-    # #return zip(t, filtered)
-    # filtered = filter_edges(samples, sample_rate, rise_time, ripple_db)
-    # return filtered
-        
-# def noisify(samples, snr_db=30.0):
-    # noise_sd = 0.5 / (10.0 ** (snr_db / 20.0))
-    # np_len = 1000
-    # np_ix = np_len
-    
-    # for s in samples:
-        # if snr_db > 80.0:
-            # yield s
-        # else:
-            # if np_ix == np_len:
-                # noise_pool = np.random.normal(0.0, noise_sd, np_len)
-                # np_ix = 0
-            
-            # noise = noise_pool[np_ix]
-            # np_ix += 1
-            
-            # yield (s[0], s[1] + noise)
-
-# def amplify(samples, gain=1.0, offset=0.0):
-    # for s in samples:
-        # yield (s[0], s[1] * gain + offset)
-
-        
-# def dropout(samples, start_time, end_time, val=0.0):
-    # for s in samples:
-        # if s[0] > start_time and s[0] < end_time:
-            # yield (s[0], val)
-        # else:
-            # yield s
-
-# def remove_excess_edges(edges):
-    # prev_state = None
-    # last_e = None
-    # for e in edges:
-        # if prev_state is None:
-            # prev_state = e[1]
-            # yield e
-        # else:
-            # if e[1] != prev_state:
-                # prev_state = e[1]
-                # last_e = None
-                # yield e
-            # else: # save last edge so we can yield it at the end
-                # last_e = e
-        
-    # if not last_e is None:
-        # yield last_e
-
 
 if __name__ == '__main__':
     print('Serial decode test tool')
@@ -481,6 +531,7 @@ if __name__ == '__main__':
     parser.add_option('-s', dest='spi', action='store_true', default=False, help='spi test')
     parser.add_option('-i', dest='i2c', action='store_true', default=False, help='i2c test')
     parser.add_option('-b', dest='usb', action='store_true', default=False, help='usb test')
+    parser.add_option('-f', dest='usb_file', action='store_true', default=False, help='usb file test')
     
     options, args = parser.parse_args()
     
@@ -499,3 +550,7 @@ if __name__ == '__main__':
     if options.usb:
         print('  Testing USB')
         test_usb()
+        
+    if options.usb_file:
+        print('  Testing USB file')
+        test_diff_usb_file()
