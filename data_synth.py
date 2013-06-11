243e54e7f1b4e411de9f6fefde75e7c5da4044e6
Kevin <kevinpt@yahoo.com>
2013-06-11 16:38:12 -0400

    Added PS/2

--- data_synth.py
+++ data_synth.py
@@ -21,22 +21,28 @@ from ripyl.protocol.spi import *
 from ripyl.protocol.uart import *
 from ripyl.protocol.i2c import *
 from ripyl.protocol.usb import *
-from ripyl.stats import OnlineStats
+from ripyl.protocol.ps2 import *
+from ripyl.util.stats import OnlineStats
+
+from test.test_support import read_bin_file
+import ripyl.util.plot as rplot
+
+import ripyl.protocol.lm73 as lm73
 
 
-def read_bin_file(fname):
-    import struct
-    with open(fname, 'rb') as fo:
-        sample_period = struct.unpack('<d', fo.read(8))[0]
-        samples = []
-        while True:
-            s = fo.read(8)
-            if len(s) > 0:
-                samples.append(struct.unpack('<d', s)[0])
-            else:
-                break
+# def read_bin_file(fname):
+    # import struct
+    # with open(fname, 'rb') as fo:
+        # sample_period = struct.unpack('<d', fo.read(8))[0]
+        # samples = []
+        # while True:
+            # s = fo.read(8)
+            # if len(s) > 0:
+                # samples.append(struct.unpack('<d', s)[0])
+            # else:
+                # break
     
-        return (samples, sample_period)
+        # return (samples, sample_period)
 
         
 def remove_short_se0s(diff_edges, min_se0_time):
@@ -197,7 +203,7 @@ def test_usb_file():
 
 
 def test_usb():
-    speed = USBSpeed.FullSpeed
+    speed = USBSpeed.HighSpeed
 
     p = USBTokenPacket(USBPID.TokenIn, 0x7F, 1, speed=speed)
     p2 = USBTokenPacket(USBPID.TokenIn, 124, 1, delay=30.0e-7, speed=speed)
@@ -218,10 +224,28 @@ def test_usb():
     packets = [p, p2, p6, p5]
     packets = [p4, p8, p9, p9, p4]
     
-    dp, dm = zip(*list(usb_synth(packets, 1.0e-7, 3.0e-7)))
     
-    dp = list(remove_excess_edges(iter(dp)))
-    dm = list(remove_excess_edges(iter(dm)))
+    packets = []
+    packets.append(USBTokenPacket(USBPID.TokenOut, 0xc, 0xf, speed=speed)) # Packet 'O Death
+    packets.append(USBDataPacket(USBPID.Data1, [93, 107, 59], speed=speed, delay=1.0e-7))
+    #packets.append(USBTokenPacket(USBPID.TokenOut, 0x72, 0x0, speed=speed))
+    #packets.append(USBDataPacket(USBPID.Data1, [58, 15], speed=speed))
+    #packets.append(USBEXTPacket(USBPID.EXT, 0x4f, 0x7, 0xb, 0x2b5, speed=speed))
+    
+    packets = []
+    packets.append(USBHandshakePacket(USBPID.PRE, speed=USBSpeed.FullSpeed))
+    packets.append(USBDataPacket(USBPID.Data1, [93, 107, 59], speed=USBSpeed.LowSpeed))
+    packets[-1].swap_jk = True
+    
+    
+    dp, dm = usb_synth(packets, 1.0e-7, 3.0e-7)
+    
+    #dp = list(remove_excess_edges(iter(dp)))
+    #dm = list(remove_excess_edges(iter(dm)))
+    dp = list(dp)
+    dm = list(dm)
+    
+    #print('@@@ DM:', dm)
     
     sr = USBClockPeriod[speed] / 10.0
     dm_t, dm_wf = zip(*list(edges_to_sample_stream(iter(dm), sr)))
@@ -251,6 +275,67 @@ def test_usb():
     plt.ylim(-0.05, 2.25)
     plt.show()
 
+def test_i2c_file():
+    #sck_samples, sample_period, start_time = read_bin_file('test/data/i2c_init_sck.bin')
+    #sda_samples, sample_period, start_time = read_bin_file('test/data/i2c_init_sda.bin')
+    sck_samples, sample_period, start_time = read_bin_file('test/data/bad_scl_2.bin')
+    sda_samples, sample_period, start_time = read_bin_file('test/data/bad_sda_2.bin')
+    time_samples = [i*sample_period for i in xrange(len(sck_samples))]
+    
+    sck_s = zip(time_samples, sck_samples)
+    sda_s = zip(time_samples, sda_samples)
+    
+    
+    # plt.clf()
+    # dp_s2 = [s + 4.0 for s in dp_samples]
+    # plt.plot(time_samples, dp_s2)
+    # plt.plot(time_samples, dm_samples)
+
+    
+    # logic = find_logic_levels(iter(sck_s))
+    # hyst = 0.4
+    # dp_e = list(find_edges(iter(dp_s), logic, hysteresis=hyst))
+    # dm_e = list(find_edges(iter(dm_s), logic, hysteresis=hyst))
+    
+    # dp_e_t, dp_e_wf = zip(*list(edges_to_sample_stream(iter(dp_e), sample_period, 1.0e-6)))
+    # dm_e_t, dm_e_wf = zip(*list(edges_to_sample_stream(iter(dm_e), sample_period, 1.0e-6)))
+    
+    # plt.plot(dp_e_t, dp_e_wf)
+    # plt.plot(dm_e_t, dm_e_wf)
+    
+    # plt.show()
+    
+    
+    #records_it = usb_decode(iter(dp_s), iter(dm_s), stream_type=StreamType.Samples)
+    records_it = i2c_decode(iter(sck_s), iter(sda_s), logic_levels=None) #(0.0,3.3))
+    records = list(records_it)
+
+    print('*************** decoded records', len(records))    
+    # decoded_msg = ''
+    # for r in records:
+        # if r.kind == 'I2C byte':
+            # decoded_msg += chr(r.data)
+            
+    lm_tfers = list(lm73.lm73_decode(iter(records)))
+    for tfer in lm_tfers:
+        temp = tfer.temperature
+        if temp is not None:
+            temp = '{} C'.format(temp)
+        else:
+            temp = ''
+
+        print('{} {}'.format(tfer, temp))
+            
+    print(records[:10])
+    title = 'I2C from file'
+    rplot.i2c_plot({'scl':sck_s, 'sda':sda_s}, records, title, label_format='hex')
+            
+
+    # for r in records:
+        # if r.kind == 'USB packet':
+            # print('{}, {}'.format((r.start_time, r.end_time), r.packet))
+        # elif r.kind == 'USB error':
+            # print('{}, {}'.format((r.start_time, r.end_time), r))
 
 def test_i2c():
     clk_freq = 100.0e3
@@ -258,7 +343,7 @@ def test_i2c():
     transfers = []
     transfers.append(I2CTransfer(I2C.Write, 0x23, [1,2,3, 4]))
     transfers.append(I2CTransfer(I2C.Write, 0x183, [5, 6, 240]))
-    scl, sda = zip(*list(i2c_synth(transfers, clk_freq, idle_start=3.0e-5, idle_end=3.0e-5)))
+    scl, sda = i2c_synth(transfers, clk_freq, idle_start=3.0e-5, idle_end=3.0e-5)
     
     scl = list(remove_excess_edges(iter(scl)))
     sda = list(remove_excess_edges(iter(sda)))
@@ -333,15 +418,14 @@ def test_spi():
         cs_s = list(noisify(edges_to_sample_stream(iter(cs), sr)))
         
         #print('                  SEL >>> ', cs_s[-1])
-        
         records = spi_decode(iter(clk_s), iter(mosi_s), iter(cs_s), cpol=cpol, cpha=cpha, lsb_first=True)
         
         records = list(records)
-        
+            
         ct, clk_wf = zip(*clk_s)
         mt, mosi_wf = zip(*mosi_s)
         st, cs_wf = zip (*cs_s)
-        
+            
         logic = find_logic_levels(iter(clk_s), max_samples=5000, buf_size=2000)
         clk_e = list(find_edges(iter(clk_s), logic, hysteresis=0.4))
         mosi_e = list(find_edges(iter(mosi_s), logic, hysteresis=0.4))
@@ -375,6 +459,108 @@ def test_spi():
             ax.add_patch(rect)    
     
     plt.show()
+
+
+def test_ps2():
+    clock_freq = 10.0e3
+    
+    dirs = [PS2Dir.DeviceToHost] * 4
+    dirs[1] = PS2Dir.HostToDevice
+
+    clk, data = ps2_synth([3, 4, 5, 120], dirs, clock_freq, 4.0 / clock_freq, 10.0 / clock_freq)
+    
+    #print('CLK:', clk)
+    #print( 'MOSI:', mosi)
+    clk = list(remove_excess_edges(iter(clk)))
+    data = list(remove_excess_edges(iter(data)))
+    
+    use_edges = False
+    
+
+    if use_edges:
+        records = ps2_decode(iter(clk), iter(data), stream_type=StreamType.Edges)
+        records = list(records)
+        
+        print('$$$ DATA', data)
+        print('$$$ CLK', clk)
+        
+        ct, clk_wf = zip(*list(edges_to_sample_stream(iter(clk), 0.1)))
+        dt, data_wf = zip(*list(amplify(edges_to_sample_stream(iter(data), 0.1), 0.8, 0.1)))
+
+        plt.clf()
+        plt.plot(ct, clk_wf)
+        plt.plot(dt, data_wf)
+        plt.ylim(-0.05, 1.25)
+        
+    else:
+    
+        sr = 1.0 / (20.0 * clock_freq)
+        # #print('>>>>>', len(list(edges_to_sample_stream(iter(clk), sr))))
+        # ct, clk_wf = zip(*list(edges_to_sample_stream(iter(clk), sr)))
+        # print('logic>', find_logic_levels(noisify(edges_to_sample_stream(iter(clk), sr)), max_samples=10000))
+        # plt.plot(ct, clk_wf)
+        # plt.show()
+
+        clk_s = list(edges_to_sample_stream(iter(clk), sr))
+        #clk_s = list(noisify(edges_to_sample_stream(iter(clk), sr), 30.0))
+        data_s = list(noisify(edges_to_sample_stream(iter(data), sr)))
+        
+        #print('                  SEL >>> ', cs_s[-1])
+
+        if 1:
+            records = ps2_decode(iter(clk_s), iter(data_s))
+            
+            records = list(records)
+        
+        ct, clk_wf = zip(*clk_s)
+        dt, data_wf = zip(*data_s)
+        
+        logic = find_logic_levels(iter(clk_s), max_samples=5000, buf_size=2000)
+        clk_e = list(find_edges(iter(clk_s), logic, hysteresis=0.4))
+        data_e = list(find_edges(iter(data_s), logic, hysteresis=0.4))
+        
+        print('$$$ DATA', data_e)
+        print('$$$ CLK', clk_e)
+        #ce_t, clk_e_wf = zip(*list(clk_e))
+        
+        # #print(clk_wf[:100])
+
+        plt.clf()
+        plt.plot(ct, clk_wf)
+        plt.plot(dt, data_wf)
+        #plt.plot(ce_t, clk_e_wf)
+        plt.ylim(-0.05, 1.25)
+
+    print('Decoded:', [str(r) for r in records])
+        
+    ax = plt.axes()
+    text_height = 1.05
+    rect_top = 1.15
+    rect_bot = -0.05
+    for r in records:
+        if r.kind == 'PS/2 frame':
+            plt.text((r.start_time + r.end_time) / 2.0, text_height, hex(r.data))
+            color = 'orange' if r.nested_status() < 200 else 'red'
+            rect = pts.Rectangle((r.start_time, rect_bot), width=r.end_time - r.start_time, height=rect_top - rect_bot, facecolor=color,  alpha=0.2)
+            ax.add_patch(rect)
+            sr = r.subrecords[0]
+            rect = pts.Rectangle((sr.start_time, rect_bot + 0.02), width=sr.end_time - sr.start_time, height=rect_top - rect_bot - 0.12, facecolor='red',  alpha=0.3)
+            ax.add_patch(rect)
+
+            sr = r.subrecords[1]
+            rect = pts.Rectangle((sr.start_time, rect_bot + 0.02), width=sr.end_time - sr.start_time, height=rect_top - rect_bot - 0.12, facecolor='blue',  alpha=0.3)
+            ax.add_patch(rect)
+
+            sr = r.subrecords[2]
+            rect = pts.Rectangle((sr.start_time, rect_bot + 0.02), width=sr.end_time - sr.start_time, height=rect_top - rect_bot - 0.12, facecolor='red',  alpha=0.3)
+            ax.add_patch(rect)
+
+            sr = r.subrecords[3]
+            rect = pts.Rectangle((sr.start_time, rect_bot + 0.02), width=sr.end_time - sr.start_time, height=rect_top - rect_bot - 0.12, facecolor='green',  alpha=0.3)
+            ax.add_patch(rect)
+    
+    plt.show()
+
 
 def test_uart():
 
@@ -543,8 +729,10 @@ if __name__ == '__main__':
     parser.add_option('-u', dest='uart', action='store_true', default=False, help='uart test')
     parser.add_option('-s', dest='spi', action='store_true', default=False, help='spi test')
     parser.add_option('-i', dest='i2c', action='store_true', default=False, help='i2c test')
+    parser.add_option('-j', dest='i2c_file', action='store_true', default=False, help='i2c file')
     parser.add_option('-b', dest='usb', action='store_true', default=False, help='usb test')
     parser.add_option('-f', dest='usb_file', action='store_true', default=False, help='usb file test')
+    parser.add_option('-p', dest='ps2', action='store_true', default=False, help='ps2 test')
     
     options, args = parser.parse_args()
     
@@ -560,6 +748,10 @@ if __name__ == '__main__':
         print('  Testing I2C')
         test_i2c()
 
+    if options.i2c_file:
+        print('  Testing I2C file')
+        test_i2c_file()
+
     if options.usb:
         print('  Testing USB')
         test_usb()
@@ -567,3 +759,9 @@ if __name__ == '__main__':
     if options.usb_file:
         print('  Testing USB file')
         test_diff_usb_file()
+
+    if options.ps2:
+        print('  Testing PS/2')
+        test_ps2()
+
+
