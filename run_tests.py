6fb60a79cdefbcc7b14615706b0bbdaf3fc132d3
Kevin <kevinpt@yahoo.com>
2013-08-14 20:53:40 -0400

    Add command line arg to control XML reporting

--- run_tests.py
+++ run_tests.py
@@ -4,10 +4,24 @@ This is the equivalent of running: python -m unittest discover
 '''
 
 import xmlrunner
+from optparse import OptionParser
 
 __unittest = True
 
 from unittest.main import main, TestProgram, USAGE_AS_MAIN
 TestProgram.USAGE = USAGE_AS_MAIN
 
-main(module=None, argv=['foo', 'discover'], testRunner=xmlrunner.XMLTestRunner(output='test/test-reports')) 
+
+parser = OptionParser()
+parser.add_option('-x', '--xml-report', dest='xml_report', action='store_true', default=False, help='enable XMLTestRunner')
+
+options, args = parser.parse_args()
+
+if options.xml_report:
+    test_runner = xmlrunner.XMLTestRunner(output='test/test-reports')
+    print 'Running tests with XMLTestRunner'
+else:
+    test_runner = None
+
+
+main(module=None, argv=['foo', 'discover'], testRunner=test_runner) 
