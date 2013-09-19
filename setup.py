ecd256821120364e6873a80569a6ba439628ca42
Kevin <kevinpt@yahoo.com>
2013-09-19 13:34:37 -0400

    Improved cython configuration with build_py subclass

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+
+from __future__ import print_function
+
 import distribute_setup
 distribute_setup.use_setuptools()
 
@@ -24,15 +27,24 @@ if version is None:
     raise RuntimeError('Unable to find version string in file: {0}'.format(version_file))
 
 
+cython_exists = False
+cmdclass = {}
 
 try:
     from Cython.Distutils import build_ext
-    cython_exists = True
-    cmdclass = {'build_ext': build_ext }
-except ImportError:
-    cython_exists = False
-    cmdclass = {}
+    from Cython.Compiler.Version import version as cy_version
+    from pkg_resources import parse_version
 
+    # Check cython version
+    min_cy_version = '0.19'
+    if parse_version(cy_version) < parse_version(min_cy_version):
+        print('Older Cython version {} found. Need at least {}.'.format(cy_version, min_cy_version))
+    else:
+        print('Cython version {} found. Build enabled.'.format(cy_version))
+        cython_exists = True
+        cmdclass = {'build_ext': build_ext }
+except ImportError:
+    pass
 
 use_cython = False if '--without-cython' in sys.argv else True
 cython_prebuild = False
@@ -51,9 +63,9 @@ def find_files(pattern, path):
 def find_cy_modules():
     '''Find Cython modules'''
     # Strip extensions from module names
-    cy_root = os.path.dirname(__file__)
+    cy_root = os.getcwd()
     cy_files = [os.path.splitext(fn)[0] for fn in find_files('*.pyx', cy_root)]
-    #print '### cy_files:', cy_files
+    #print('### cy_files:', cy_files)
 
     # Convert slashes to dotted module notation
     trans = string.maketrans('/\\', '..')
@@ -64,21 +76,8 @@ def find_cy_modules():
 
     return cy_modules
 
-def write_config(cfg_path, use_cython, cython_prebuild):
-    if sys.hexversion < 0x3000000:
-        import ConfigParser as cp
-    else:
-        import configparser as cp
 
-    config = cp.ConfigParser()
-    config.add_section('setup')
-    config.set('setup', 'use_cython', str(use_cython))
-    config.set('setup', 'cython_prebuild', str(cython_prebuild))
 
-    with open(cfg_path, 'w') as fh:
-        config.write(fh)
-
-ext_modules = []
 features = {}
 #if 'install' in sys.argv or 'build' in sys.argv or 'build_ext' in sys.argv:
 if cython_exists:
@@ -87,6 +86,7 @@ if cython_exists:
     # Find all cython modules
     cy_module_names = find_cy_modules()
 
+    ext_modules = []
     cy_root = ['ripyl', 'cython']
     for mname in cy_module_names:
         full_mname = '.'.join([e for x in [cy_root, [mname]] for e in x])
@@ -102,8 +102,51 @@ if cython_exists:
 
 #print '$$$ use_cython', use_cython, cython_prebuild
 
-if 'install' in sys.argv:
-    write_config(os.path.join('ripyl', 'ripyl.cfg'), use_cython, cython_prebuild)
+#if 'install' in sys.argv:
+#    write_config(os.path.join('ripyl', 'ripyl.cfg'), use_cython, cython_prebuild)
+
+
+# Subclass build_py command to add our own hook to write a config file
+from setuptools.command.build_py import build_py as _build_py
+
+class build_py(_build_py):
+    #user_options = _build_py.user_options
+    #boolean_options = _build_py.boolean_options
+
+    def initialize_options(self):
+        _build_py.initialize_options(self)
+
+    def finalize_options(self):
+        _build_py.finalize_options(self)
+
+    def run(self):
+        cfg_path = os.path.join(self.build_lib, 'ripyl', 'ripyl.cfg')
+        #print('$$$$ mkpath:', os.path.dirname(cfg_path))
+        self.mkpath(os.path.dirname(cfg_path))
+        print('Writing Ripyl configuration file: {}'.format(cfg_path))
+        self.write_config(cfg_path, use_cython, cython_prebuild)
+
+        return _build_py.run(self)
+
+    #def get_module_outfile(self, build_dir, package, module):
+        #print('$$$$ get outfile:', build_dir, package, module)
+        #return _build_py.get_module_outfile(self, build_dir, package, module)
+
+    def write_config(self, cfg_path, use_cython, cython_prebuild):
+        if sys.hexversion < 0x3000000:
+            import ConfigParser as cp
+        else:
+            import configparser as cp
+
+        config = cp.ConfigParser()
+        config.add_section('setup')
+        config.set('setup', 'use_cython', str(use_cython))
+        config.set('setup', 'cython_prebuild', str(cython_prebuild))
+
+        with open(cfg_path, 'w') as fh:
+            config.write(fh)
+
+cmdclass['build_py'] = build_py
 
 
 
